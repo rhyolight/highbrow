cbbd68040295fcd1f5745e33e16ef04fdf459d01
Matthew Taylor <matt@numenta.org>
2017-06-21 14:46:31 -0700

    Restructuring tests.

---
+++ tests/renderable-tests.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+const assert = require("chai").assert
+const expect = require("chai").expect
+
+// const Renderable = require("../src/renderable")
+
+/*
+Basic 3D translation.
+*/

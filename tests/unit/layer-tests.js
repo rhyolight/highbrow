eca5e26cdea14af96052757cb46344bafbec3a9d
Matthew Taylor <matt@numenta.org>
2017-06-21 23:18:11 -0700

    Added layer cell placement and tests

--- tests/unit/layer-tests.js
+++ tests/unit/layer-tests.js
@@ -9,28 +9,30 @@ const Layer = require("../../src/layer")
 
 describe("Layer Unit Tests", () => {
 
-    describe("without mini-columns", () => {
-        const config = {
-            name: "layer 1",
-            miniColumns: false,
-            neuronCount: 100,
-            dimensions: {
-                x: 10, y: 10, z: 1
-            }
+    const config = {
+        name: "layer 1",
+        miniColumns: false,
+        neuronCount: 1000,
+        dimensions: {
+            x: 10, y: 10, z: 10
         }
-        let layer = new Layer(config)
+    }
+    let layer = new Layer(config)
+
+    describe("regarding state updates", () => {
 
         it("returns neurons in global index order", () => {
             // We'll turn on every 6th neuron globally.
             let activeIndices = []
-            for (let i=0; i<100; i++) {
+            for (let i = 0; i < config.neuronCount; i++) {
                 if (i % 6 == 0) activeIndices.push(i)
             }
             layer.update(activeIndices)
             let count = 0
             for (var neuron of layer.getNeurons()) {
-                let expectedState = count++ % 6 == 0 ? NeuronState.active : NeuronState.inactive
+                let expectedState = count % 6 == 0 ? NeuronState.active : NeuronState.inactive
                 expect(neuron.state).to.equal(expectedState)
+                count++
             }
         })
 
@@ -38,7 +40,7 @@ describe("Layer Unit Tests", () => {
             // Now we'll turn on every 7th neuron globally, which should
             // override the previous state.
             let activeIndices = []
-            for (let i=0; i<100; i++) {
+            for (let i = 0; i < config.neuronCount; i++) {
                 if (i % 7 == 0) activeIndices.push(i)
             }
             layer.update(activeIndices)
@@ -48,6 +50,58 @@ describe("Layer Unit Tests", () => {
                 expect(neuron.state).to.equal(expectedState)
             }
         })
+
     })
+
+    describe("regarding neuron 3D placement", () => {
+
+        /*
+        Active cell indices returned from HTM systems generally are ordered with
+        mini-columns grouped together. Since we want to render mini-columns as
+        depth, they need to be in the Z dimension, and that's why we translate
+        the cell indices into the z dimension first.
+        */
+
+        it("has z as the first dimension", () => {
+            for (let zcount = 0; zcount < config.dimensions.z; zcount++) {
+                let neuron = layer.getNeuronByIndex(zcount)
+                expect(neuron.getOrigin()).to.deep.equal({
+                    x: 0, y: 0, z: zcount
+                })
+            }
+        })
+
+        it("has x as the second dimension", () => {
+            for (let xcount = 0; xcount < config.dimensions.x; xcount++) {
+                for (let zcount = 0; zcount < config.dimensions.z; zcount++) {
+                    let globalIndex = xcount * config.dimensions.z + zcount
+                    let neuron = layer.getNeuronByIndex(globalIndex)
+                    // console.log(neuron.toString())
+                    // console.log("xcount: %s zcount: %s", xcount, zcount)
+                    expect(neuron.getOrigin()).to.deep.equal({
+                        x: xcount, y: 0, z: zcount
+                    })
+                }
+            }
+        })
+
+        it("has y as the third dimension", () => {
+            for (let ycount = 0; ycount < config.dimensions.y; ycount++) {
+                for (let xcount = 0; xcount < config.dimensions.x; xcount++) {
+                    for (let zcount = 0; zcount < config.dimensions.z; zcount++) {
+                        let globalIndex = ycount * config.dimensions.x * config.dimensions.z
+                                        + xcount * config.dimensions.z
+                                        + zcount
+                        let neuron = layer.getNeuronByIndex(globalIndex)
+                        expect(neuron.getOrigin()).to.deep.equal({
+                            x: xcount, y: ycount, z: zcount
+                        })
+                    }
+                }
+            }
+        })
+
+    })
+
 
 })

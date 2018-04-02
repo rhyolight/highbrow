8175a9625b6401fffe5aed405d01c2f0094849b5
rhyolight <rhyolight@gmail.com>
2018-04-02 15:56:59 -0700

    Tweaking publication script

--- bin/publish.sh
+++ bin/publish.sh
@@ -5,9 +5,11 @@ tmp="/tmp"
 npm run build
 
 rm -rf "$tmp/lowbrow"
-mkdir "$tmp/lowbrow"
+mkdir "$tmp/lowbrow/"
+mkdir "$tmp/lowbrow/binaries"
 
-cp out/* "$tmp/lowbrow/."
+cp out/* "$tmp/lowbrow/binaries/."
+mv node_modules "$tmp/lowbrow/."
 
 git checkout gh-pages
 
@@ -19,3 +21,4 @@ git commit -m "Publishing latest to gh-pages"
 git push origin gh-pages
 
 git checkout master
+mv "$tmp/lowbrow/node_modules" .
